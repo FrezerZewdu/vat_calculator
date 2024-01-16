9faa6a154c1917b89120661432fe341369331401
Frezer <frezerzewdu@gmail.com>
2024-01-16 16:46:16 +0400

    date picker component installed

--- vat-calculator-frontend/src/main.ts
+++ vat-calculator-frontend/src/main.ts
@@ -2,6 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vClickOutside from "click-outside-vue3";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 import "./assets/tailwind.css";
 
-createApp(App).use(router).use(vClickOutside).mount("#app");
+createApp(App)
+  .use(router)
+  .use(vClickOutside)
+  .component("VueDatePicker", VueDatePicker)
+  .mount("#app");
