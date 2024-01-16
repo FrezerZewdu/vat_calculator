80fd2c515909a020920f402c773a6f6533528d08
Frezer <frezerzewdu@gmail.com>
2024-01-16 14:10:30 +0400

    click outside directive installed

--- vat-calculator-frontend/src/main.ts
+++ vat-calculator-frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vClickOutside from "click-outside-vue3";
 import "./assets/tailwind.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(vClickOutside).mount("#app");
