4a871a7cedc94325b42f2ed0f5a2ce0b581fdc33
Frezer <frezerzewdu@gmail.com>
2024-01-16 17:54:43 +0400

    pinia store installed

--- vat-calculator-frontend/src/main.ts
+++ vat-calculator-frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 import vClickOutside from "click-outside-vue3";
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
@@ -9,5 +10,6 @@ import "./assets/tailwind.css";
 createApp(App)
   .use(router)
   .use(vClickOutside)
+  .use(createPinia())
   .component("VueDatePicker", VueDatePicker)
   .mount("#app");
