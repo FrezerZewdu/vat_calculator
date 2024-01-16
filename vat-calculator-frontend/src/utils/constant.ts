887af0ea08621dc81ad5f75fb9900fd9e4b47968
Frezer <frezerzewdu@gmail.com>
2024-01-16 17:54:01 +0400

    const file created

---
+++ vat-calculator-frontend/src/utils/constant.ts
@@ -0,0 +1,8 @@
+export const DATETIME_OPTIONS = {
+  weekday: "long" as const,
+  year: "numeric" as const,
+  month: "short" as const,
+  day: "numeric" as const,
+  hour: "numeric" as const,
+  minute: "numeric" as const,
+};
