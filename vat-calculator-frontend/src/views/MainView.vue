4a4eedd03a2bc2dffad111524bd3bf5002417bce
Frezer <frezerzewdu@gmail.com>
2024-01-22 19:43:04 +0300

    main view initalized

---
+++ vat-calculator-frontend/src/views/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid xl:grid-cols-6 lg:grid-cols-4 grid-cols-1">
+    <NavBarVue :choosenMenu="selectedMenu" />
+    <div class="col-span-5 lg:col-span-3 col-span-1 h-screen overflow-y-auto">
+      <router-view @menuSelected="(value) => (selectedMenu = value)" />
+    </div>
+  </div>
+</template>
+<script>
+import NavBarVue from "@/layout/NavBar.vue";
+export default {
+  components: {
+    NavBarVue,
+  },
+  data() {
+    return {
+      selectedMenu: 1,
+    };
+  },
+};
+</script>
