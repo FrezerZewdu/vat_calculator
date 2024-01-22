1eddfd3dad85db047ce73824b2cb1f02f17af84f
Frezer <frezerzewdu@gmail.com>
2024-01-22 20:32:22 +0300

    css bug fix

--- vat-calculator-frontend/src/views/MainView.vue
+++ vat-calculator-frontend/src/views/MainView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="grid xl:grid-cols-6 lg:grid-cols-4 grid-cols-1">
     <NavBarVue :choosenMenu="selectedMenu" />
-    <div class="col-span-5 lg:col-span-3 col-span-1 h-screen overflow-y-auto">
+    <div
+      class="xl:col-span-5 lg:col-span-3 col-span-1 h-screen overflow-y-auto"
+    >
       <router-view @menuSelected="(value) => (selectedMenu = value)" />
     </div>
   </div>
