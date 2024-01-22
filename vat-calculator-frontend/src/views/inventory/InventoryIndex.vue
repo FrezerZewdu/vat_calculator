d9dbaa702be158d409a5d1e38937250228e76a6d
Frezer <frezerzewdu@gmail.com>
2024-01-22 19:45:15 +0300

    inventory component installed

---
+++ vat-calculator-frontend/src/views/inventory/InventoryIndex.vue
@@ -0,0 +1,66 @@
+<template>
+  <LoadingPageVue :loading="loading">
+    <h2 class="text-black/70 font-medium">Inventory</h2>
+    <div class="mt-5 py-6 px-4 w-full flex justify-between bg-white rounded-lg">
+      <SearchBarVue
+        class="basis-1/3"
+        :placeholderText="'Search Item'"
+        :suggestions="suggestions"
+        @textInput="fetchSuggestions"
+        @wantedItem="search"
+      />
+      <button class="py-2 px-5 primary-button">New Item</button>
+    </div>
+    <div class="mt-5 w-full h-3/4 flex space-x-5">
+      <div class="py-6 px-4 w-3/4 bg-white rounded-lg">
+        <table class="w-full">
+          <thead class="bg-slate-50 shadow-sm rounded-t-lg">
+            <th class="text-sm font-medium text-black/70 p-2">ID</th>
+            <th class="text-sm font-medium text-black/70 p-2">NAME</th>
+            <th class="text-sm font-medium text-black/70 p-2">STOCK</th>
+            <th class="text-sm font-medium text-black/70 p-2">VAT STOCK</th>
+            <th class="text-sm font-medium text-black/70 p-2">PRICE</th>
+            <th class="text-sm font-medium text-black/70 p-2">ACTIONS</th>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+      <div class="py-6 px-4 w-1/4 bg-white rounded-lg"></div>
+    </div>
+  </LoadingPageVue>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import SearchBarVue from "@/components/common/SearchBar.vue";
+import LoadingPageVue from "../../layout/LoadingPage.vue";
+
+export default defineComponent({
+  components: {
+    LoadingPageVue,
+    SearchBarVue,
+  },
+  data() {
+    return {
+      loading: true,
+      suggestions: [],
+    };
+  },
+  methods: {
+    fetchSuggestions(searchString: string) {
+      console.log(searchString);
+      // call api to fetch suggestions
+    },
+    search(searchString: string) {
+      console.log(searchString);
+      // call api to search and replace full inventory items with searched one
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1300);
+    this.$emit("menuSelected", 2);
+  },
+});
+</script>
---
+++ vat-calculator-frontend/src/views/inventory/ItemDetail.vue
@@ -0,0 +1,49 @@
+<template>
+  <ModalBackgroundVue>
+    <div class="w-1/2 h-3/4 p-10 bg-white rounded-xl">
+      <div class="flex justify-end">
+        <!-- close icon svg -->
+        <svg
+          @click="$emit('closeModal')"
+          class="cursor-pointer"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"
+          />
+        </svg>
+      </div>
+      <div class="flex mt-5 border-t">
+        <div class="w-1/2">
+          <h3>{{ item.name }}</h3>
+        </div>
+        <div class="w-1/2 flex justify-between">
+          <!-- doughnut charts go here -->
+        </div>
+      </div>
+    </div>
+  </ModalBackgroundVue>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ItemDetailType } from "@/model/Item";
+
+import ModalBackgroundVue from "@/layout/ModalBackground.vue";
+export default defineComponent({
+  components: {
+    ModalBackgroundVue,
+  },
+  data() {
+    return {
+      item: {} as ItemDetailType,
+    };
+  },
+  async mounted() {
+    // this.item = await api
+  },
+});
+</script>
