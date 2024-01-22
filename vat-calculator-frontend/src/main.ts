4a4a841046e4ddf4f84ae05b87e19317c7f6a88f
Frezer <frezerzewdu@gmail.com>
2024-01-22 19:35:36 +0300

    filters made global

--- vat-calculator-frontend/src/main.ts
+++ vat-calculator-frontend/src/main.ts
@@ -4,12 +4,16 @@ import router from "./router";
 import { createPinia } from "pinia";
 import vClickOutside from "click-outside-vue3";
 import VueDatePicker from "@vuepic/vue-datepicker";
+import filter from "./utils/filters";
 import "@vuepic/vue-datepicker/dist/main.css";
 import "./assets/tailwind.css";
 
-createApp(App)
+const app = createApp(App);
+app
   .use(router)
   .use(vClickOutside)
   .use(createPinia())
   .component("VueDatePicker", VueDatePicker)
   .mount("#app");
+
+app.config.globalProperties.$filters = filter;
--- vat-calculator-frontend/src/utils/filters.ts
+++ vat-calculator-frontend/src/utils/filters.ts
@@ -1,7 +1,11 @@
 import { DATETIME_OPTIONS } from "./constant";
 
-export function readableDate(rawDate: string): string {
-  return rawDate != null
-    ? new Date(rawDate).toLocaleDateString("en-US", DATETIME_OPTIONS)
-    : "-";
-}
+const filter = {
+  readableDate(rawDate: string): string {
+    return rawDate != null
+      ? new Date(rawDate).toLocaleDateString("en-US", DATETIME_OPTIONS)
+      : "-";
+  },
+};
+
+export default filter;
