ce3060afb3f6e852a090bd7f1d1e64bdf2e9873a
Frezer <frezerzewdu@gmail.com>
2024-01-29 22:33:33 +0300

    route guards made per role

--- vat-calculator-frontend/src/router/index.ts
+++ vat-calculator-frontend/src/router/index.ts
@@ -6,8 +6,17 @@ import {
 } from "vue-router";
 import LoginView from "../views/LoginView.vue";
 import MainView from "../views/MainView.vue";
+import { useAuthStore } from "@/stores/Auth";
 
-export type AppRouteName = "login" | "dashboard" | "inventory";
+export type AppRouteName =
+  | "login"
+  | "dashboard"
+  | "inventory"
+  | "sales"
+  | "transactions"
+  | "accounts";
+
+const authStore = useAuthStore();
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -24,26 +33,61 @@ const routes: Array<RouteRecordRaw> = [
         path: "dashboard",
         name: "dashboard",
         component: () => import("../views/dashboard/DashboardIndex.vue"),
+        beforeEnter: (to, from, next) => {
+          if (authStore.isRole(["supAdmin", "norAdmin"])) {
+            next();
+          } else {
+            next({ name: "login" });
+          }
+        },
       },
       {
         path: "inventory",
         name: "inventory",
         component: () => import("../views/inventory/InventoryIndex.vue"),
+        beforeEnter: (to, from, next) => {
+          if (authStore.isRole(["supAdmin", "norAdmin", "finance"])) {
+            next();
+          } else {
+            next({ name: "login" });
+          }
+        },
       },
       {
         path: "sales",
         name: "sales",
         component: () => import("../views/sale/SaleIndex.vue"),
+        beforeEnter: (to, from, next) => {
+          if (authStore.isRole(["supAdmin", "norAdmin", "sales"])) {
+            next();
+          } else {
+            next({ name: "login" });
+          }
+        },
       },
       {
         path: "transactions",
         name: "transactions",
         component: () => import("../views/transaction/TransactionIndex.vue"),
+        beforeEnter: (to, from, next) => {
+          if (authStore.isRole(["supAdmin", "norAdmin", "finance"])) {
+            next();
+          } else {
+            next({ name: "login" });
+          }
+        },
       },
       {
         path: "accounts",
         name: "accounts",
         component: () => import("../views/account/AccountIndex.vue"),
+        beforeEnter: (to, from, next) => {
+          if (authStore.isRole(["supAdmin", "norAdmin"])) {
+            next();
+          } else {
+            next({ name: "login" });
+          }
+        },
       },
     ],
   },
